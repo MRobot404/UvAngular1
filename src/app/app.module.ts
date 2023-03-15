3c2441113c4c0bfbb6843305f804837ec021dc07
Oscar Escobar <qomc16@gmail.com>
2023-03-14 20:04:37 -0600

    Versión 1.1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {PanelModule} from 'primeng/panel';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatChipsModule} from '@angular/material/chips';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatSlideToggleModule,
     BrowserModule,
-    AppRoutingModule
+    ButtonModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PanelModule,
+    CardModule,
+    InputTextModule,
+    PasswordModule,
+    FormsModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
